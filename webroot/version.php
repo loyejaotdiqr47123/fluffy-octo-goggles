751ede39a280efb7bcbe22329e589a37665aba59
loyejaotdiqr47123 <102934154+loyejaotdiqr47123@users.noreply.github.com>
2022-04-07 19:17:59 +0800

    1
    1

---
+++ webroot/version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,9,1,2);
+$OC_VersionString = '10.9.1';
+$OC_Channel = 'stable';
+$OC_Edition = 'community';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2022-01-12T14:27:13+00:00 515d81b0a5aa381de766390f927b76e2a54a6861';
+$vendor = 'owncloud';
